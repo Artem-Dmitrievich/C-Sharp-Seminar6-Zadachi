1c0913a3e25844338875b620535bbf2c4b29aea6
kozlov artem <artemko1987@yandex.ru>
2022-08-15 00:58:31 +0300

    задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+*/
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = new Random().Next(100, 1000);
+
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+for (int i = 0; i < n / 2; i++)
+{
+    int x = array[i];
+    array[i] = array[n - i - 1];
+    array[n - i - 1] = x;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
