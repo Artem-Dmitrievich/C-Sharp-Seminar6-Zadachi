ab11eb6f2869133f8ad612ebf4cda94a760369c1
kozlov artem <artemko1987@yandex.ru>
2022-08-15 00:59:16 +0300

    Задача42

---
+++ Задача42/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+int n = Convert.ToInt32(Console.ReadLine());
+string end = "";
+while (n > 0)
+{
+    int x = n % 2;
+    end = Convert.ToString(x) + end;
+    n /= 2;
+}
+Console.WriteLine(end);
