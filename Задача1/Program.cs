48451081f0a11bca796495d88b4658e3687f8768
kozlov artem <artemko1987@yandex.ru>
2022-08-15 00:58:12 +0300

    задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+*/
+//i,j - x, y,строка-столбец
+int[,] array = new int[5, 5];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = Convert.ToInt32(Console.ReadLine());
+}
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine("\t");
+}
